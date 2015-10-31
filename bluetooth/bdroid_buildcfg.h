d09ea845b343d5e9ca071bf8e1d1f3e37180bcca
TheStrix <parthbhatia98@gmail.com>
2015-10-31 05:54:21 -0400

    armani: Update Bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,10 @@
 /*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +21,13 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
 #define BTM_DEF_LOCAL_NAME   "Xiaomi HM 1S"
-
-#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    16
-// Skips conn update at conn completion
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
